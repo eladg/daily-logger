69c2411b3dd17e0164a0d52b485cf1b45a58135f
eladg <ggrelad@gmail.com>
2015-06-16 13:42:56 +0300

    init sinatra

---
+++ app.rb
@@ -0,0 +1,67 @@
+# load environment
+require 'dotenv'
+require 'pry'
+require 'json'
+Dotenv.load
+
+# sinatra & global dependancies
+require 'sinatra'
+
+class ExceptionHandling
+  def initialize(app)
+    @app = app
+  end
+ 
+  def call(env)
+    begin
+      @app.call env
+    rescue => ex
+      env['rack.errors'].puts ex
+      env['rack.errors'].puts ex.backtrace.join("\n")
+      env['rack.errors'].flush 
+      
+      hash = { message: ex.to_s, app_backtrace: app_backtrace(ex)}
+      
+      # log
+      # pm_to_user hash
+
+      [500, {'Content-Type' => 'application/json'}, [ex.to_s]]
+    end
+  end
+
+  def pm_to_user(hash)
+    # unimplemented
+  end
+
+  def app_backtrace(ex)
+    ex.backtrace.select { |path| path[/rvm|rbenv|\/app\/vendor\/bundle/].nil? }
+  end
+end
+
+class SlackLogger < Sinatra::Application
+  set :environment, :production
+
+  set :root,                File.dirname(__FILE__)
+  set :public_folder,       Proc.new { File.join(root, "app", "public") }
+  set :views,               Proc.new { File.join(root, "app", "templates") }
+  set :static,              true
+
+  # disable errors because we are handling them ourselves
+  set :dump_errors,         false
+  set :raise_errors,        true
+  set :show_exceptions,     false
+
+  configure :development do
+    # maybe sometime in the future
+  end
+
+  helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+  end
+
+  get '/log' do
+    params.to_json
+  end
+
+end
